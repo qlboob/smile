7826056e58e28fbdc1a8b1ecf8c3105cc4a99bbe
qlboob <ql2008@126.com>
2016-10-18 22:52:40 +0800

    解决0 在select中被空字串选中的bug

--- lib/smilesys/tag/TagForm.php
+++ lib/smilesys/tag/TagForm.php
@@ -243,7 +243,7 @@ class TagForm extends BaseTag {
 			$ret .= '<?php foreach('.$optionV.' as $key=>$val){?>';
         	if(!empty($selected)) {
         		//0=='' 所以要区分一 下这个情况
-                $ret   .= '<?php if(isset($'.$selected.')&&($'.$selected.'==$key&&$key!==0&&\'\'!==$'.$selected.'||(is_array($'.$selected.')&&in_array($key,$'.$selected.')))){?>';
+                $ret   .= '<?php if(isset($'.$selected.')&&($'.$selected.'==$key&&($key!==0||\'\'!==$'.$selected.')||(is_array($'.$selected.')&&in_array($key,$'.$selected.')))){?>';
                 $ret   .= "<option selected=\"selected\" value=\"<?php echo $keyFun(\$key); ?>\"><?php echo $valueFun(\$val);?></option>";
                 $ret   .= "<?php }else{?><option value=\"<?php echo $keyFun(\$key);?>\"><?php echo $valueFun(\$val);?></option>";
                 $ret   .= '<?php }?>';
